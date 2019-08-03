92949f235ab1f81d001e73f8f950ec7fa12349ca
peter_van_onselen <peter.van.onselen@oracle.com>
2019-08-03 19:50:27 +0200

    generate words for silly word game

---
+++ word.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+if __name__ == '__main__':
+    words = open('words.txt', 'r').read()
+    words_list = words.split('\n')
+
+    search = sys.argv[1]
+    # looking_for = int(sys.argv[2])
+
+    looking = []
+
+    for word in words_list:
+        found = True
+        lowered_word = word.lower()
+        for c in lowered_word:
+            if c not in search or lowered_word.count(c) > 1:
+                found = False
+
+        if found:
+            looking.append(lowered_word)
+
+    sorted = {}
+    for word in looking:
+        sorted.setdefault(str(len(word)), []).append(word.lower())
+
+    print json.dumps(sorted, indent=4, sort_keys=True)
